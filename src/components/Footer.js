83f3309a48ac2e817fb7e96118f592512f2d34d3
Manpreetkaur666 <mk27982@gmail.com>
2023-05-01 11:35:35 -0400

    responsive design added

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      @copyright by Little Lemon
+    </div>
+  )
+}
+
+export default Footer
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,17 +3,41 @@ import bannerimg from '../images/bannerimg.jpg';
 
 const Main = () => {
   return (
-    <div className='banner'>
-         <div className='banner-left'>
-            <h1>Little Lemon</h1>
-            <h2>Chicago</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br></br>
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <button>Reserve a Table</button>
+    <div className='main'>
+  
+    <h1>Book a Table</h1>
+      <form method="POST">
+         <div>
+            <label for="booking_date">Booking Date</label>
+            <input type="date" id="booking_date" name="booking_date" />
          </div>
-         <div className='banner-right'>
-            <img src={bannerimg}/>
+         <div>
+            <label for="booking_people">Number of People</label>
+            <input type="range" id="booking_people" name="booking_people" min="1" max="7" value="4" oninput="this.nextElementSibling.value = this.value" />
+            <output>4</output>
          </div>
+         <div>
+            <label for="booking_loacation">Location</label>
+            <input id="booking_location" name="booking_location" list="locations" />
+            <datalist id="locations">
+                <option value="Mississauga"></option>
+                <option value="Toronto"></option>
+                <option value="Barrie"></option>
+                <option value="Ottawa"></option>
+            </datalist>
+         </div>
+         <div>
+            <fieldset id="dinning_option">
+                <label>
+                    <input type="radio" id="dinning_option" value="indoors" name="dinning_option" /> Indoors
+                </label>
+                 <label> 
+                    <input type="radio" id="dinning_option" value="outdoor" name="dinning_option" /> Outdoor
+                 </label>       
+            </fieldset>
+         </div>
+         <button className='btnBookNow'>Book Now</button>
+      </form>
     </div>
   )
 }
